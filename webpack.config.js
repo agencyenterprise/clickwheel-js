d6ae3bb1ba801f026bc4baff4f0a64e27a2babc1
Jean Mayer <jean.mayer@ae.studio>
2022-03-03 00:00:15 -0300

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/ClickWheel.js",
+  output: {
+    path: path.resolve("lib"),
+    filename: "ClickWheel.js",
+    libraryTarget: "commonjs2",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+    },
+  },
+  externals: {
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React",
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM",
+    },
+  },
+};
